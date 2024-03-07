0e49d3f051a708f74ea4b42b7a0617c47060e539
manhhungpm <hungnm.nuce60@gmail.com>
2024-03-07 18:11:32 +0700

    update js sdk version

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,7 +36,7 @@ const Layout = ({ preview, children, headerContent, headerClassName }: Props) =>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> */}
       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
       {/* <script src="https://static.airflex.io/sdk/js/1.0.2/airflex.js"></script> */}
-      <script src="https://d2fpeiluuf92qr.cloudfront.net/sdk/js/1.0.3/airflex.js"></script>
+      <script src="https://d2fpeiluuf92qr.cloudfront.net/sdk/js/1.0.4/airflex.js"></script>
       <Script id="AirflexJS-analytics">
         {`
          AirflexJS.create("310797", {devmode: true});
